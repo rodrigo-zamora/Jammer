54b54a2b7448b5c6323958bd6845dead82083804
José Jaime Gutiérrez Martínez <42259245+JoseJaimeGtz@users.noreply.github.com>
2022-05-07 15:14:13 -0500

    edit button on listCard

--- front-end/src/app/components/list-card/list-card.component.ts
+++ front-end/src/app/components/list-card/list-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ListService } from '../list.service';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DialogOverviewExampleDialogComponent } from '../dialog-overview-example-dialog/dialog-overview-example-dialog.component';
+
 
 @Component({
   selector: 'app-list-card',
@@ -10,7 +13,22 @@ export class ListCardComponent implements OnInit {
 
   @Input() list: any;
 
-  constructor(public lists: ListService) { }
+  constructor(public lists: ListService, public dialog: MatDialog) { }
+
+  animal: string | undefined;
+  name: string | undefined;
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialogComponent, {
+      width: '550px',
+      data: {name: this.name, animal: this.animal},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
 
   ngOnInit(): void {
   }
